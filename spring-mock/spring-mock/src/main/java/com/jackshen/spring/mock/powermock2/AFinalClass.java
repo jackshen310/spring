1f627df0ded6fbfd936af3d93d74bfcd71ff740d
jackshen <1217457206@qq.com>
2014-11-07 20:13:15 +0800

    Add powermock sample

---
+++ spring-mock/spring-mock/src/main/java/com/jackshen/spring/mock/powermock2/AFinalClass.java
@@ -0,0 +1,9 @@
+package com.jackshen.spring.mock.powermock2;
+
+public final class AFinalClass
+{
+    public final String echoString(String s)
+    {
+        return s;
+    }
+}
---
+++ spring-mock/spring-mock/src/main/java/com/jackshen/spring/mock/powermock2/AStaticClass.java
@@ -0,0 +1,9 @@
+package com.jackshen.spring.mock.powermock2;
+
+public class AStaticClass
+{
+    public static final String echoString(String s)
+    {
+        return s;
+    }
+}
